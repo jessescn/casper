1675acb0adb796164eb2bf2b2895d1ea75127d21
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2019-08-17 16:27:01 -0300

    prepare to heroku deploy

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const APP_ROUTES: Routes = [
+    {path:'', component: HomeComponent, canActivate: [AuthGuard]},
     {path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     {path: '**', component: HomeComponent, canActivate: [AuthGuard]}
